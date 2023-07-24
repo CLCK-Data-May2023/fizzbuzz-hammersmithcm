352396e2026109b4efc71413eba6f518b3fa8657
Christina Hammersmith <hammersmithcmx@gmail.com>
2023-07-24 19:57:59 -0400

    assignment complete

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,10 @@
-# add your code here
+for x in range(1,101):
+    if x %3==0 and x %5==0:
+        print('FizzBuzz')
+    if x %3==0:
+        print('Fizz')
+    if x %5==0:
+        print('Buzz')
+    else:
+        print(x)# add your code here
 
